dbc463834dadd11b0ebc46eb83a52c4e04c27cab
arti-jansari <artijansari@gmail.com>
2022-12-24 17:30:12 +0530

    calculator

---
+++ src/Calculator/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './App.css'
+
+
+const App = () => {
+  const [result ,setResult] =useState('');
+  const [save,setSave]=useState()
+ const onhandleclick = (e)=>{
+
+  setResult(result.concat(e.target.value))
+  console.log(result)
+ } 
+ const handleclear = ()=>{
+  setResult('')
+ }
+ const handlecalculate = ()=>{
+  setResult(eval(result))
+ }
+
+ const handleclearvalue = (e)=>{
+setSave(result)
+  setResult(save.pop())
+ }
+  return (
+    <div className='calc'>
+      <input type="text" name='input' placeholder='0' value ={result} id='input' />
+      <input type='button' className='button' value='7'  onClick={onhandleclick} />
+      <input type='button' className='button' value='8'  onClick={onhandleclick} />
+      <input type='button' className='button' value='9'  onClick={onhandleclick} />
+      <input type='button' className='button' value='*'  onClick={onhandleclick} />
+      <input type='button' className='button' value='4'  onClick={onhandleclick} />
+      <input type='button' className='button' value='5'  onClick={onhandleclick} />
+      <input type='button' className='button' value='6'  onClick={onhandleclick} />
+      <input type='button' className='button' value='+'  onClick={onhandleclick} />
+      <input type='button' className='button' value='1'  onClick={onhandleclick} />
+      <input type='button' className='button' value='2'  onClick={onhandleclick} />
+      <input type='button' className='button' value='3'  onClick={onhandleclick} />
+      <input type='button' className='button' value='-'  onClick={onhandleclick} />
+      <input type='button'  className='button' value='AC'  onClick={handleclear} />
+      <input type='button' className='button' value='/'  onClick={onhandleclick} />
+      <input type='button' className='button' value='='  onClick={handlecalculate} />
+      <input type='button' className='button' value='Clear'  onClick={handleclearvalue} />
+      
+     
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Calculator'
+// import App from './App';
+// import App from './Card_fetchapi';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
