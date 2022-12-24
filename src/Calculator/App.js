bc7ca986a930d2471be6d7633754b6f1a1445109
arti-jansari <artijansari@gmail.com>
2022-12-24 21:34:21 +0530

    clear

--- src/Calculator/App.js
+++ src/Calculator/App.js
@@ -4,11 +4,15 @@ import './App.css'
 
 const App = () => {
   const [result ,setResult] =useState('');
-  const [save,setSave]=useState()
+  // const [newdata ,setNewdata] =useState('');
+
+  const onhandlechange = (e)=>{
+setResult(e.target.value)
+  }
  const onhandleclick = (e)=>{
 
   setResult(result.concat(e.target.value))
-  console.log(result)
+  // console.log(result)
  } 
  const handleclear = ()=>{
   setResult('')
@@ -17,13 +21,19 @@ const App = () => {
   setResult(eval(result))
  }
 
- const handleclearvalue = (e)=>{
-setSave(result)
-  setResult(save.pop())
+ const handleclearvalue = ()=>{
+ 
+ const newdata=result.split('').pop()
+ setResult(result.toString())
+ console.log(result)
+//  setResult(result)
  }
+
+
+ 
   return (
     <div className='calc'>
-      <input type="text" name='input' placeholder='0' value ={result} id='input' />
+      <input type="text" name='input' placeholder='0' value ={result} id='input' onChange={onhandlechange} />
       <input type='button' className='button' value='7'  onClick={onhandleclick} />
       <input type='button' className='button' value='8'  onClick={onhandleclick} />
       <input type='button' className='button' value='9'  onClick={onhandleclick} />
